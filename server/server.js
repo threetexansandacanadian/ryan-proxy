992cbc159337dd383e140dec22230d0ab883869c
ryden <ryferr8122@gmail.com>
2019-04-12 16:10:31 -0500

    server serves static dist folder

--- server/server.js
+++ server/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('dist'))
+
 app.get('/', (req, res) => {
-  res.send('I am a callback!');
+  res.send();
 })
 
 app.listen(port, console.log('Server ready on port:', port));
